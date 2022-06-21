f8170c3481f38ca798afcc0c7e762b0de36bc98b
Darren Darrenlemyacrawford LeMya Crawford <darrenlemyacrawford@Darrens-Air.fios-router.home>
2022-06-20 20:14:13 -0400

    adding adtl retiree tables/csv

--- schema.sql
+++ schema.sql
@@ -37,8 +37,8 @@ CREATE TABLE dept_emp (
 	emp_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (dept_no, emp_no)
 );
 CREATE TABLE title (
