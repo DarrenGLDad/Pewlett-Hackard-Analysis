8a8d00b2e8a9abae92eeee8bca0ec710d930ce7c
DarrenGLDad <100239100+DarrenGLDad@users.noreply.github.com>
2022-06-22 18:35:13 -0400

    Adding query
    query -number of employees by their most recent job title

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -8,3 +8,7 @@ INTO nameyourtable
 FROM _______
 WHERE _______
 ORDER BY _____, _____ DESC;
+
+--Write another query to retrieve the number of employees by their most recent job title who are about to
+SELECT * FROM unique_titles
+
